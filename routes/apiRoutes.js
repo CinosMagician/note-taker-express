6060925530d74e2c0a1031db07a241a13af32cda
Lachlan <lachyhughes@bigpond.com>
2024-05-19 23:21:39 +0930

    Added some helpers uuid and readAndAppend, readFromFile to help with reading a file aswell as reading then appening a file, the notepad texts. Added basic note taking and will look into how to delete a specific note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,33 @@
 const apiRoute = require('express').Router();
-const path = require('path');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+apiRoute.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    console.log(`Reading notes db...`);
+});
+
+apiRoute.get('/id', (req, res) => {
+    readFromFile('./db/id.json').then((data) => res.json(JSON.parse(data)))
+    console.log(`Reading id number`);
+});
+
+apiRoute.post('/notes', (req, res) => {
+    const { title, text} = req.body;
+
+  
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid()
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
+  });
 
 module.exports = apiRoute;
