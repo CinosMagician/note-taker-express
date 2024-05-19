6e6881adc373c09f45c0414a818103e7c7324d52
Lachlan <lachyhughes@bigpond.com>
2024-05-19 21:58:40 +0930

    Added beginning of js files needed for back end work, including the start of the api and html route js files along with the server.js file. Added starter code to each

---
+++ routes/apiRoutes.js
@@ -0,0 +1,4 @@
+const apiRoute = require('express').Router();
+const path = require('path');
+
+module.exports = apiRoute;
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const api = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use('/api', api);
